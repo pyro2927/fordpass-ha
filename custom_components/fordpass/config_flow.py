bbcd80f6af5a59f9b4f06577411462ef3f657fdf
Maikel Bloemendal <TheLizard@users.noreply.github.com>
2024-07-11 21:59:32 +0200

    Resolved all but 1 flake8 messages The only remaining is the lengthy URL in config_flow.py

--- custom_components/fordpass/config_flow.py
+++ custom_components/fordpass/config_flow.py
@@ -35,18 +35,17 @@ _LOGGER = logging.getLogger(__name__)
 DATA_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_USERNAME): str,
-        #vol.Required(CONF_PASSWORD): str,
+        # vol.Required(CONF_PASSWORD): str,
         vol.Required(REGION): vol.In(REGION_OPTIONS),
     }
 )
-
-
 
 VIN_SCHEME = vol.Schema(
     {
         vol.Required(VIN, default=""): str,
     }
 )
+
 
 @callback
 def configured_vehicles(hass):
@@ -55,6 +54,7 @@ def configured_vehicles(hass):
         entry.data[VIN]
         for entry in hass.config_entries.async_entries(DOMAIN)
     }
+
 
 async def validate_token(hass: core.HomeAssistant, data):
     _LOGGER.debug(data)
@@ -65,17 +65,13 @@ async def validate_token(hass: core.HomeAssistant, data):
         vehicle.generate_tokens,
         data["tokenstr"],
         data["code_verifier"]
-        )
+    )
 
     if results:
         _LOGGER.debug("Getting Vehicles")
         vehicles = await(hass.async_add_executor_job(vehicle.vehicles))
         _LOGGER.debug(vehicles)
         return vehicles
-
-    
-
-
 
 
 async def validate_input(hass: core.HomeAssistant, data):
@@ -95,26 +91,27 @@ async def validate_input(hass: core.HomeAssistant, data):
     try:
         if result:
             vehicles = await(hass.async_add_executor_job(vehicle.vehicles))
-    except Exception as ex:
+    except Exception:
         vehicles = None
-    #except Exception as ex:
-    #    raise InvalidAuth from ex
+    # except Exception as ex:
+    #     raise InvalidAuth from ex
 
-    #result3 = await hass.async_add_executor_job(vehicle.vehicles)
-    # Disabled due to API change
-    #vinfound = False
-    #for car in result3:
-    #    if car["vin"] == data[VIN]:
-    #        vinfound = True
-    #if vinfound == False:
-    #    _LOGGER.debug("Vin not found in account, Is your VIN valid?")
+    # result3 = await hass.async_add_executor_job(vehicle.vehicles)
+    # # Disabled due to API change
+    # vinfound = False
+    # for car in result3:
+    #     if car["vin"] == data[VIN]:
+    #         vinfound = True
+    # if vinfound == False:
+    #     _LOGGER.debug("Vin not found in account, Is your VIN valid?")
     if not result:
         _LOGGER.error("Failed to authenticate with fordpass")
         raise CannotConnect
 
     # Return info that you want to store in the config entry.
     return vehicles
-    #return {"title": f"Vehicle ({data[VIN]})"}
+    # return {"title": f"Vehicle ({data[VIN]})"}
+
 
 async def validate_vin(hass: core.HomeAssistant, data):
     configPath = hass.config.path("custom_components/fordpass/" + data[CONF_USERNAME] + "_fordpass_token.txt")
@@ -129,6 +126,7 @@ async def validate_vin(hass: core.HomeAssistant, data):
     if not test:
         raise InvalidVin
     return False
+
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for FordPass."""
@@ -151,14 +149,14 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             except CannotConnect:
                 print("EXCEPT")
                 errors["base"] = "cannot_connect"
-        
+
         return self.async_show_form(
             step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
-    
+
     async def async_step_token(self, user_input=None):
         errors = {}
-        
+
         if user_input is not None:
             try:
                 token = user_input["tokenstr"]
@@ -178,10 +176,10 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     if self.vehicles is None:
                         return await self.async_step_vin()
                     return await self.async_step_vehicle()
-                    
+
                 else:
                     errors["base"] = "invalid_token"
-                
+
             except CannotConnect:
                 print("EXCEPT")
                 errors["base"] = "cannot_connect"
@@ -190,32 +188,28 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             _LOGGER.debug("Region")
             _LOGGER.debug(self.region)
             return self.async_show_form(
-                step_id="token", data_schema=
-                    vol.Schema(
+                step_id="token", data_schema=vol.Schema(
                     {
                         vol.Optional("url", default=self.generate_url(self.region)): str,
                         vol.Required("tokenstr"): str,
                     }
-                    )
-                , errors=errors
+                ), errors=errors
             )
-        
+
     def check_token(self, token):
         if "fordapp://userauthorized/?code=" in token:
             return True
         return False
-    
-
 
     def generate_url(self, region):
         _LOGGER.debug(REGIONS[region])
         code1 = ''.join(random.choice(string.ascii_lowercase) for i in range(43))
         code_verifier = self.generate_hash(code1)
         self.login_input["code_verifier"] = code1
-        
-        url = f"{REGIONS[region]["locale_url"]}/4566605f-43a7-400a-946e-89cc9fdb0bd7/B2C_1A_SignInSignUp_{REGIONS[region]["locale"]}/oauth2/v2.0/authorize?redirect_uri=fordapp://userauthorized&response_type=code&max_age=3600&code_challenge={code_verifier}&code_challenge_method=S256&scope=%2009852200-05fd-41f6-8c21-d36d3497dc64%20openid&client_id=09852200-05fd-41f6-8c21-d36d3497dc64&ui_locales={REGIONS[region]["locale"]}&language_code={REGIONS[region]["locale"]}&country_code={REGIONS[region]["locale_short"]}&ford_application_id={REGIONS[region]["region"]}"
-        
+
+        url = f"{REGIONS[region]['locale_url']}/4566605f-43a7-400a-946e-89cc9fdb0bd7/B2C_1A_SignInSignUp_{REGIONS[region]['locale']}/oauth2/v2.0/authorize?redirect_uri=fordapp://userauthorized&response_type=code&max_age=3600&code_challenge={code_verifier}&code_challenge_method=S256&scope=%2009852200-05fd-41f6-8c21-d36d3497dc64%20openid&client_id=09852200-05fd-41f6-8c21-d36d3497dc64&ui_locales={REGIONS[region]['locale']}&language_code={REGIONS[region]['locale']}&country_code={REGIONS[region]['locale_short']}&ford_application_id={REGIONS[region]['region']}"
         return url
+
     def base64_url_encode(self, data):
         """Encode string to base64"""
         return urlsafe_b64encode(data).rstrip(b'=')
@@ -225,11 +219,12 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         hashengine = hashlib.sha256()
         hashengine.update(code.encode('utf-8'))
         return self.base64_url_encode(hashengine.digest()).decode('utf-8')
+
     def validNumber(self, phone_number):
         pattern = re.compile("^([+]\d{2})?\d{10}$", re.IGNORECASE)
         pattern2 = re.compile("^([+]\d{2})?\d{9}$", re.IGNORECASE)
-        return pattern.match(phone_number) is not None or pattern2.match(phone_number) is not None  
-        
+        return pattern.match(phone_number) is not None or pattern2.match(phone_number) is not None
+
     async def async_step_vin(self, user_input=None):
         """Handle manual VIN entry"""
         errors = {}
@@ -246,20 +241,20 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             except Exception:
                 errors["base"] = "unknown"
 
-            if vehicle :
+            if vehicle:
                 return self.async_create_entry(title=f"Vehicle ({user_input[VIN]})", data=self.login_input)
 
             # return self.async_create_entry(title=f"Enter VIN", data=self.login_input)
         _LOGGER.debug(self.login_input)
         return self.async_show_form(step_id="vin", data_schema=VIN_SCHEME, errors=errors)
-    
+
     async def async_step_vehicle(self, user_input=None):
         if user_input is not None:
             _LOGGER.debug("Checking Vehicle is accessible")
             self.login_input[VIN] = user_input["vin"]
             _LOGGER.debug(self.login_input)
             return self.async_create_entry(title=f"Vehicle ({user_input[VIN]})", data=self.login_input)
-        
+
         _LOGGER.debug(self.vehicles)
 
         configured = configured_vehicles(self.hass)
@@ -278,10 +273,10 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_abort(reason="no_vehicles")
         return self.async_show_form(
             step_id="vehicle",
-            data_schema = vol.Schema(
-            { vol.Required(VIN): vol.In(avaliable_vehicles)}
+            data_schema=vol.Schema(
+                {vol.Required(VIN): vol.In(avaliable_vehicles)}
             ),
-            errors = {}
+            errors={}
         )
 
     @staticmethod
@@ -314,7 +309,7 @@ class OptionsFlow(config_entries.OptionsFlow):
             ): vol.In(DISTANCE_UNITS),
             vol.Optional(
                 DISTANCE_CONVERSION_DISABLED,
-                default = self.config_entry.options.get(
+                default=self.config_entry.options.get(
                     DISTANCE_CONVERSION_DISABLED, DISTANCE_CONVERSION_DISABLED_DEFAULT
                 ),
             ): bool,
@@ -324,7 +319,7 @@ class OptionsFlow(config_entries.OptionsFlow):
                     UPDATE_INTERVAL, UPDATE_INTERVAL_DEFAULT
                 ),
             ): int,
-            
+
         }
 
         return self.async_show_form(step_id="init", data_schema=vol.Schema(options))
@@ -332,6 +327,7 @@ class OptionsFlow(config_entries.OptionsFlow):
 
 class CannotConnect(exceptions.HomeAssistantError):
     """Error to indicate we cannot connect."""
+
 
 class InvalidToken(exceptions.HomeAssistantError):
     """Error to indicate there is invalid token."""
@@ -343,6 +339,7 @@ class InvalidAuth(exceptions.HomeAssistantError):
 
 class InvalidVin(exceptions.HomeAssistantError):
     """Error to indicate the wrong vin"""
+
 
 class InvalidMobile(exceptions.HomeAssistantError):
     """Error to no mobile specified for South African Account"""
